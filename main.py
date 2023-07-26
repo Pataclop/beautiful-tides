b37bdb7be4884cbfb616eeda7de741f5ae75eaea
Pataclop <escure.f@gmail.com>
2023-07-26 19:24:41 +0200

    mieux
    plus qu'a intégrer les dates

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def ecrire_texte_dans_csv(texte, nom_fichier):
     with open(nom_fichier, 'w') as fichier:
         fichier.write(texte)
 
-def clean (soupe) :
+def clean (soup) :
     all_spans = soup.find_all('span')
     # Créer un nouvel arbre HTML contenant uniquement les balises <span>
     new_soup = BeautifulSoup()
@@ -83,140 +83,133 @@ def ligne_commence_par_mot(liste_mots, ligne):
     return False
 
 
-url = "https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/le-verdon-sur-mer-1036/septembre-2023"  # Remplacez cela par l'URL de la page que vous souhaitez scraper
-
-response = requests.get(url)
-if response.status_code != 200:
-    print("Error: %s" % response.status_code)
-    
-    
-    
-soup = BeautifulSoup(response.content, "html.parser")
-t = clean(soup)
-t = t.replace(',', '\n')
-t = unidecode(t)
-t = output_text = remove_lines_until_marker(t, "101-129")
-t = output_text = remove_lines_until_marker(t, "101-129")
-t = output_text = remove_lines_after_marker(t, "3201")
-t = t.replace("\n ", "\n")[1:]
-t = t.replace("Lune\n", "Lune : ")
-t = t.replace("Saint\nSaint", "Saint")
-t = aligne_basse(t)
-t = t.replace(" Maree haute", "\nMaree haute")
-t = t.replace(" Lune :", "\nLune :")
-t = aligne_haute(t)
-t = t.replace(" Mar", "\nMar")
-t = t.replace(" Lune :", "\nLune :")
-t = t.replace("\nCoucher\n", " ")
-t = t.replace("Lever\n", "Soleil : ")
-
-t = t.replace("lundi","lun")
-t = t.replace("mardi","mar")
-t = t.replace("mercredi", "mer")
-t = t.replace("jeudi","jeu")
-t = t.replace("vendredi", "ven")
-t = t.replace("samedi", "sam")
-t = t.replace("dimanche", "dim")
-    
-lines = t.split('\n')
-tab = np.empty((124, 5), dtype=object)
-i = 0
-date = "rien 0"
-lune = None
-for l in lines:
-    if ligne_commence_par_mot(semaine, l):
-        date = l
-    if l.startswith("Maree"):
-        tab[i][0] = date
-        l = l[12:]
-        tmp = l.split(" ")
-        tab[i][1] = tmp[0]
-        tab[i][2] = tmp[1]
-        if len(tmp)==3 :
-            tab[i][3] = tmp[2]
-        i=i+1
-    if l.startswith("Lune"):
-        tab[i-1][4] = l[7:]
-
-
-
-ecrire_texte_dans_csv(t, "out.txt ")
-
-
-
-
-
-
-
-
-
-
-
-
-# Liste des hauteurs (exemples)
-hauteurs = np.zeros(124)
-for i in range(len(hauteurs)):
-    if tab[i][2] is not None :
-        hauteurs[i] = float(tab[i][2][:-1])
-
-hauteurs = np.delete(hauteurs, np.where(hauteurs == 0.0))
-moyenne_hauteur = np.mean(hauteurs)
-
-heures = np.empty((124), dtype=object)
-for i in range(len(hauteurs)):
-    if tab[i][1] is not None :
-        heures[i] = tab[i][1]
-heures = np.delete(heures, np.where(heures == ""))
-
-coeficient = np.empty((124), dtype=object)
-for i in range(len(coeficient)):
-    if tab[i][3] is not None :
-        coeficient[i] = (tab[i][3])
-
-# Créer une liste d'abscisses pour les hauteurs
-abscisses = [i*5 for i in range(len(hauteurs))]
-
-# Créer la figure et les axes
-fig, ax = plt.subplots()
-
-# Tracer les hauteurs sous forme de segments noirs
-for i in range(len(hauteurs) - 1):
-    ax.plot([abscisses[i], abscisses[i+1]], [hauteurs[i], hauteurs[i+1]], color='black', linewidth=3)
-
-
-for x, y in zip(abscisses, hauteurs):
-    if y > moyenne_hauteur :
-        ax.text(x, y+0.2, f'{y}', ha='center', va='bottom', fontname='Arial', fontsize=12, color='grey', weight='bold')
-    else :
-        ax.text(x, y-0.2, f'{y}', ha='center', va='top', fontname='Arial', fontsize=12, color='grey', weight='bold')
+def draw(link, nom):
+    response = requests.get(link)
+    if response.status_code != 200:
+        print("Error: %s" % response.status_code)
+        exit(1)
         
-for x, y, h in zip(abscisses, hauteurs, heures):
-    if y > moyenne_hauteur :
-        ax.text(x, y+0.4, h, ha='center', va='bottom', fontname='Arial', fontsize=12, color='black', weight='bold')
-    else :
-        ax.text(x, y-0.6, h, ha='center', va='bottom', fontname='Arial', fontsize=12, color='black', weight='bold')
+        
+        
+    soup = BeautifulSoup(response.content, "html.parser")
+    t = clean(soup)
+    t = t.replace(',', '\n')
+    t = unidecode(t)
+    t = output_text = remove_lines_until_marker(t, "101-129")
+    t = output_text = remove_lines_until_marker(t, "101-129")
+    t = output_text = remove_lines_after_marker(t, "3201")
+    t = t.replace("\n ", "\n")[1:]
+    t = t.replace("Lune\n", "Lune : ")
+    t = t.replace("Saint\nSaint", "Saint")
+    t = aligne_basse(t)
+    t = t.replace(" Maree haute", "\nMaree haute")
+    t = t.replace(" Lune :", "\nLune :")
+    t = aligne_haute(t)
+    t = t.replace(" Mar", "\nMar")
+    t = t.replace(" Lune :", "\nLune :")
+    t = t.replace("\nCoucher\n", " ")
+    t = t.replace("Lever\n", "Soleil : ")
 
-last_coef = 0
-for i in range(5):
-    if coeficient[i] is not None and int(coeficient[i]) >10:
-        last_coef = coeficient[i]   
-for x, y, c in zip(abscisses, hauteurs, coeficient):
-    if c is not None and int(c) > 10 :
-        last_coef = c
-    if y > moyenne_hauteur :
-        ax.text(x, moyenne_hauteur-0.3, str(last_coef), ha='center', va='bottom', fontname='Arial', fontsize=14, color='black', weight='bold')
+    t = t.replace("lundi","lun")
+    t = t.replace("mardi","mar")
+    t = t.replace("mercredi", "mer")
+    t = t.replace("jeudi","jeu")
+    t = t.replace("vendredi", "ven")
+    t = t.replace("samedi", "sam")
+    t = t.replace("dimanche", "dim")
+        
+    lines = t.split('\n')
+    tab = np.empty((124, 5), dtype=object)
+    i = 0
+    date = "rien 0"
+    lune = None
+    for l in lines:
+        if ligne_commence_par_mot(semaine, l):
+            date = l
+        if l.startswith("Maree"):
+            tab[i][0] = date
+            l = l[12:]
+            tmp = l.split(" ")
+            tab[i][1] = tmp[0]
+            tab[i][2] = tmp[1]
+            if len(tmp)==3 :
+                tab[i][3] = tmp[2]
+            i=i+1
+        if l.startswith("Lune"):
+            tab[i-1][4] = l[7:]
 
 
-#for x, y, i in  zip(abscisses, hauteurs, coeficient):
-#    ax.text(x, 3.5, i, ha='center', va='bottom', fontname='Arial', fontsize=12, color='blue', weight='bold')
-## Tracer les points pour les hauteurs
-#ax.plot(abscisses, hauteurs)
-#ax.axis('off')
-#plt.xlim(0, 300)
 
-plt.axis('off')
-largeur_pouces = 80
-hauteur_pouces = 6
-fig = plt.gcf()
-fig.set_size_inches(largeur_pouces, hauteur_pouces)
-plt.savefig('mon_graphe.png', dpi=300, bbox_inches='tight')
+    # Liste des hauteurs (exemples)
+    hauteurs = np.zeros(124)
+    for i in range(len(hauteurs)):
+        if tab[i][2] is not None :
+            hauteurs[i] = float(tab[i][2][:-1])
+
+    hauteurs = np.delete(hauteurs, np.where(hauteurs == 0.0))
+    moyenne_hauteur = np.mean(hauteurs)
+
+    heures = np.empty((124), dtype=object)
+    for i in range(len(hauteurs)):
+        if tab[i][1] is not None :
+            heures[i] = tab[i][1]
+    heures = np.delete(heures, np.where(heures == ""))
+
+    coeficient = np.empty((124), dtype=object)
+    for i in range(len(coeficient)):
+        if tab[i][3] is not None :
+            coeficient[i] = (tab[i][3])
+
+    # Créer une liste d'abscisses pour les hauteurs
+    abscisses = [i*5 for i in range(len(hauteurs))]
+
+    # Créer la figure et les axes
+    fig, ax = plt.subplots()
+
+    # Tracer les hauteurs sous forme de segments noirs
+    for i in range(len(hauteurs) - 1):
+        ax.plot([abscisses[i], abscisses[i+1]], [hauteurs[i], hauteurs[i+1]], color='black', linewidth=3)
+
+
+    for x, y in zip(abscisses, hauteurs):
+        if y > moyenne_hauteur :
+            ax.text(x, y+0.2, f'{y}', ha='center', va='bottom', fontname='Arial', fontsize=12, color='grey', weight='bold')
+        else :
+            ax.text(x, y-0.2, f'{y}', ha='center', va='top', fontname='Arial', fontsize=12, color='grey', weight='bold')
+            
+    for x, y, h in zip(abscisses, hauteurs, heures):
+        if y > moyenne_hauteur :
+            ax.text(x, y+0.4, h, ha='center', va='bottom', fontname='Arial', fontsize=12, color='black', weight='bold')
+        else :
+            ax.text(x, y-0.6, h, ha='center', va='bottom', fontname='Arial', fontsize=12, color='black', weight='bold')
+
+    last_coef = 0
+    for i in range(5):
+        if coeficient[i] is not None and int(coeficient[i]) >10:
+            last_coef = coeficient[i]   
+    for x, y, c in zip(abscisses, hauteurs, coeficient):
+        if c is not None and int(c) > 10 :
+            last_coef = c
+        if y > moyenne_hauteur :
+            ax.text(x, moyenne_hauteur-0.3, str(last_coef), ha='center', va='bottom', fontname='Arial', fontsize=14, color='black', weight='bold')
+
+
+    #for x, y, i in  zip(abscisses, hauteurs, coeficient):
+    #    ax.text(x, 3.5, i, ha='center', va='bottom', fontname='Arial', fontsize=12, color='blue', weight='bold')
+    ## Tracer les points pour les hauteurs
+    #ax.plot(abscisses, hauteurs)
+    #ax.axis('off')
+    #plt.xlim(0, 300)
+
+    plt.axis('off')
+    largeur_pouces = 80
+    hauteur_pouces = 6
+    fig = plt.gcf()
+    fig.set_size_inches(largeur_pouces, hauteur_pouces)
+    plt.savefig(nom, dpi=300, bbox_inches='tight')
+    
+mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+url = "https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/le-verdon-sur-mer-1036/" 
+
+for m in mois :
+    draw(url+m+"-2023", m+"-2023.png")
