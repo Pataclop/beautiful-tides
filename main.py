2c110c4093e23448bec01dbe5c1c66390eae5a3c
Pataclop <escure.f@gmail.com>
2023-07-26 15:22:49 +0200

    scrap

--- main.py
+++ main.py
@@ -1,7 +1,114 @@
-#https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/la-tranche-sur-mer-1080/mai-2024
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import requests
+import csv
+from bs4 import BeautifulSoup
+from unidecode import unidecode
+import re
+
+
+
+
+def aligne_basse(chaine):
+    # Créer un modèle de regex pour trouver "Maree basse" suivie de la prochaine lettre "M" ou "L"
+    modele = re.compile(r"Maree basse(.*?[ML])", re.DOTALL)
+    # Utiliser la méthode sub() pour supprimer les retours à la ligne dans la correspondance
+    chaine_modifiee = modele.sub(lambda match: "Maree basse" + match.group(1).replace('\n', ' '), chaine)
+    return chaine_modifiee
+
+def aligne_haute(chaine):
+    # Créer un modèle de regex pour trouver "Maree basse" suivie de la prochaine lettre "M" ou "L"
+    modele = re.compile(r"Maree haute(.*?[ML])", re.DOTALL)
+    # Utiliser la méthode sub() pour supprimer les retours à la ligne dans la correspondance
+    chaine_modifiee = modele.sub(lambda match: "Maree haute" + match.group(1).replace('\n', ' '), chaine)
+    return chaine_modifiee
+
+def ecrire_texte_dans_csv(texte, nom_fichier):
+    with open(nom_fichier, 'w') as fichier:
+        fichier.write(texte)
+
+def clean (soupe) :
+    all_spans = soup.find_all('span')
+    # Créer un nouvel arbre HTML contenant uniquement les balises <span>
+    new_soup = BeautifulSoup()
+    new_html = new_soup.new_tag('html')
+    new_soup.append(new_html)
+    new_body = new_soup.new_tag('body')
+    new_html.append(new_body)
+
+    # Ajouter les balises <span> trouvées dans le nouvel arbre HTML
+    all_spans = soup.find_all('span')
+
+    # Obtenir les contenus des balises <span> dans une liste
+    span_contents = [span.get_text() for span in all_spans]
+
+    # Joindre les contenus par des virgules pour créer la chaîne finale
+    cleaned_text = ', '.join(span_contents)
+
+    return cleaned_text
+
+
+def remove_lines_until_marker(text, marker):
+    lines = text.split("\n")
+    output_lines = []
+    found_marker = False
+
+    for line in lines:
+        if found_marker:
+            output_lines.append(line)
+        if marker in line:
+            found_marker = True
+
+    result = "\n".join(output_lines)
+    return result
+
+def remove_lines_after_marker(text, marker):
+    lines = text.split("\n")
+    output_lines = []
+    found_marker = False
+
+    for line in lines:
+        if marker in line:
+            found_marker = True
+        if not found_marker:
+            output_lines.append(line)
+    result = "\n".join(output_lines)
+    return result
+
+url = "https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/le-verdon-sur-mer-1036/aout-2023"  # Remplacez cela par l'URL de la page que vous souhaitez scraper
+
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    t = clean(soup)
+    t = t.replace(',', '\n')
+    t = unidecode(t)
+    t = output_text = remove_lines_until_marker(t, "101-129")
+    t = output_text = remove_lines_until_marker(t, "101-129")
+    t = output_text = remove_lines_after_marker(t, "3201")
+    t = t.replace("\n ", "\n")[1:]
+    t = t.replace("Lune\n", "Lune : ")
+    t = t.replace("Saint\nSaint", "Saint")
+    t = aligne_basse(t)
+    t = t.replace(" Maree haute", "\nMaree haute")
+    t = t.replace(" Lune :", "\nLune :")
+    t = aligne_haute(t)
+    t = t.replace(" Mar", "\nMar")
+    t = t.replace(" Lune :", "\nLune :")
+    t = t.replace("\nCoucher\n", " ")
+    t = t.replace("Lever\n", "Soleil : ")
+    ecrire_texte_dans_csv(t, "out.txt ")
+
+
+
+
+
+
+
+
+
+
 
 # Liste des hauteurs (exemples)
 hauteurs = [4.89, 2.10, 4.99, 2.05, 4.89, 2.10, 4.99, 2.05]
@@ -34,7 +141,7 @@ ax.axis('off')
 #plt.show()
 
 temp_image_path = "temp_plot.png"
-plt.savefig(temp_image_path, dpi=300, bbox_inches='tight')  # dpi contrôle la résolution de l'image
+plt.savefig(temp_image_path, dpi=100, bbox_inches='tight')  # dpi contrôle la résolution de l'image
 
 # Charger l'image temporaire avec OpenCV
 image = cv2.imread(temp_image_path)
@@ -44,8 +151,8 @@ import os
 os.remove(temp_image_path)
 
 # Afficher l'image (optionnel)
-cv2.imshow("Figure OpenCV", image)
-cv2.waitKey(0)
+#cv2.imshow("Figure OpenCV", image)
+#cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Enregistrer l'image OpenCV dans un format spécifique (par exemple JPEG)
