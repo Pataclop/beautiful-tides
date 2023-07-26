7a5983bb8fd5ab2e4ef9430f6c55f24a44cf3a39
Pataclop <escure.f@gmail.com>
2023-07-26 12:19:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/la-tranche-sur-mer-1080/mai-2024
-
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 
 # Liste des hauteurs (exemples)
-hauteurs = [4.89, 5.65, 5.78, 1.3]
-heures = ["12:34", "12:55", "2:34", "23:55"]
+hauteurs = [4.89, 2.10, 4.99, 2.05, 4.89, 2.10, 4.99, 2.05]
+heures = ["12:34", "12:55", "2:34", "23:55","12:34", "12:55", "2:34", "23:55"]
+coeficient = ["90", "92", "94", "93","90", "92", "94", "93"]
+
 # Créer une liste d'abscisses pour les hauteurs
 abscisses = [i*50 for i in range(len(hauteurs))]
 
@@ -21,17 +24,31 @@ for x, y in zip(abscisses, hauteurs):
     
 for x, y, h in zip(abscisses, hauteurs, heures):
     ax.text(x, y+0.4, h, ha='center', va='bottom', fontname='Arial', fontsize=12, color='blue', weight='bold')
-    
+
+for x, y, i in  zip(abscisses, hauteurs, coeficient):
+    ax.text(x, 3.5, i, ha='center', va='bottom', fontname='Arial', fontsize=12, color='blue', weight='bold')
 # Tracer les points pour les hauteurs
 ax.plot(abscisses, hauteurs)
 ax.axis('off')
 
-# Définir les limites des axes
-#ax.set_xlim(0, len(hauteurs)*50)
-#ax.set_ylim(0, max(hauteurs)*1.2)
+#plt.show()
 
-# Afficher la grille
-#ax.grid(True)
+temp_image_path = "temp_plot.png"
+plt.savefig(temp_image_path, dpi=300, bbox_inches='tight')  # dpi contrôle la résolution de l'image
 
-# Afficher le graphique
-plt.show()
+# Charger l'image temporaire avec OpenCV
+image = cv2.imread(temp_image_path)
+
+# Supprimer l'image temporaire
+import os
+os.remove(temp_image_path)
+
+# Afficher l'image (optionnel)
+cv2.imshow("Figure OpenCV", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Enregistrer l'image OpenCV dans un format spécifique (par exemple JPEG)
+output_image_path = "output_plot.jpg"
+cv2.imwrite(output_image_path, image)
+
